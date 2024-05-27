adb40b4f24df83dd7df8cb884a4de6a45a608776
EK <elku.cryptos@gmail.com>
2024-05-27 12:57:32 +0530

    working_version_table_included_formatted_shortcut_keys

--- app_gui.py
+++ app_gui.py
@@ -21,7 +21,7 @@ layout_forward = [
 control_buttons = [
     [pg.Button("PLAY", key="-PLAY-"), pg.Button("MARK", key="-MARK-")],
     [pg.Column(layout_forward, element_justification="center"), pg.Column(layout_backward, element_justification="center")],   
-    [pg.Button("Close", key="-CLOSE_BTN-")]
+    [pg.Button("CLOSE", key="-CLOSE_BTN-")]
 ]
 
 info_panel = [
@@ -35,11 +35,23 @@ info_panel = [
         pg.Text("00:00", font=("Arial", 11), size=(6,1), border_width=1, relief="solid", key="-TOTAL_TIME-")],
 ]
 
+marked_ts_array = [[]]
+timestamp_table = [pg.Table(values=[marked_ts_array], headings=["Marked @", "Duration"], max_col_width=25, background_color='lightblue',
+                    auto_size_columns=True,
+                    display_row_numbers=True,
+                    justification='right',
+                    text_color='black',
+                    num_rows=10,
+                    alternating_row_color='lightyellow',
+                    key='-TABLE-',
+                    tooltip='Timestamps at which lines are marked in the audio file')],
+
 layout = [
-    [pg.Text("Audio file name", text_color="black", background_color="white", size=(100,2), key="-AUDIO_FILE_NAME-"),
+    [pg.Text("C:/Users/wipin/Desktop/DATA DRIVE/Vedas/2 Yajur veda/Krishna Yajur Veda/KYV001.mp3", text_color="black", \
+            background_color="white", size=(100,2), key="-AUDIO_FILE_NAME-"),
         pg.FileBrowse('Browse', auto_size_button=True, key="-AUDIO_FILE-", \
         initial_folder="C:\\Users\\wipin\\Desktop\\DATA DRIVE\\Vedas")],
-    [pg.Column(control_buttons, element_justification="center"), pg.Push(), pg.Column(info_panel, element_justification="right")]
+    [pg.Column(control_buttons, element_justification="center"), pg.Column(timestamp_table), pg.Push(), pg.Column(info_panel, element_justification="right")]
     
 ]
 
@@ -60,8 +72,38 @@ def update_time_boxes():
     window["-CURR_TIME-"].update(curr_time)
     window["-REM_TIME-"].update(rem_time)
 
-window = pg.Window("Music Lyrics Marker", layout)
-audio_file_fullname = "C:\\Users\\wipin\\Desktop\\DATA DRIVE\\Vedas\\2 Yajur veda\\Krishna Yajur Veda\\KYV001.mp3"
+def adjust_num_lines_marked():
+    curr_time = playback.curr_pos
+    index = len(marked_ts_array)
+    list_ts = [x[0] for x in marked_ts_array]
+    while index >= 1:
+        if(curr_time <= list_ts[-1]):
+            list_ts.pop()
+            marked_ts_array.pop()
+            index = len(list_ts)
+        else:
+            break
+    global num_lines_marked 
+    num_lines_marked = len(list_ts)
+    window["-NUM_LINES_MARKED-"].update(len(list_ts))
+    format_floats_for_table()
+
+def format_floats_for_table():
+    marked_ts_string = []
+    if(len(marked_ts_array) <= 1):
+        return
+    for i in range(len(marked_ts_array)):
+        marked_ts_string.append([f"{marked_ts_array[i][0]:.3f}", f"{marked_ts_array[i][1]:.3f}"])
+    window['-TABLE-'].update(values=list(reversed(marked_ts_string)))
+
+window = pg.Window("Music Lyrics Marker", layout, finalize=True)
+
+# short cut keys
+window.bind("<p>", "PLAY-KEY")  # press "p" to play/pause
+window.bind("<m>", "MARK-KEY")  # press "p" to mark
+
+
+audio_file_fullname = "C:/Users/wipin/Desktop/DATA DRIVE/Vedas/2 Yajur veda/Krishna Yajur Veda/KYV001.mp3"
 playback = Playback() # creates an object for managing playback of a single audio file
 play_clicked_once = 0
 total_audio_duration = 0
@@ -74,14 +116,12 @@ while True:
         update_time_boxes()
     if event is None or event == "-CLOSE_BTN-":
         break
-    elif event == "-AUDIO_FILE-":
-        values["-AUDIO_FILE_NAME-"] = audio_file_fullname
-    elif event == "-PLAY-":
+    # elif event == "-AUDIO_FILE-":
+    #     values["-AUDIO_FILE_NAME-"] = audio_file_fullname
+    elif event == "-PLAY-" or event == "PLAY-KEY":
         if(play_clicked_once == 0):
-            list_ts = []
             play_clicked_once = 1
-            print("aaaa")
-            audio_file_fullname = values["-AUDIO_FILE-"]
+            audio_file_fullname = window["-AUDIO_FILE_NAME-"].get()
             playback.load_file(audio_file_fullname) 
             total_audio_duration = playback.duration
             window["-TOTAL_TIME-"].update(sec2_timestring(total_audio_duration))
@@ -94,26 +134,34 @@ while True:
             playback.pause()
     elif event == "-BACK_5_SECS-":
         playback.seek(playback.curr_pos-5)
+        adjust_num_lines_marked()
     elif event == "-BACK_1_MIN-":
         playback.seek(playback.curr_pos-60)
+        adjust_num_lines_marked()
     elif event == "-BACK_10_MIN-":
         playback.seek(playback.curr_pos-600)
+        adjust_num_lines_marked()
     elif event == "-FORW_5_SECS-":
         playback.seek(playback.curr_pos+5)
     elif event == "-FORW_1_MIN-":
         playback.seek(playback.curr_pos+60)
     elif event == "-FORW_10_MIN-":
         playback.seek(playback.curr_pos+600)
-    elif event == "-MARK-":
+    elif event == "-MARK-" or event == "MARK-KEY":
         # the line cannot be < 1 second long
         if(time.time() - last_marked_ts > 1):
-            num_lines_marked += 1
-            list_ts.append(playback.curr_pos)  # list of timestamps.
+            curr_time = playback.curr_pos
+             # list of timestamps and their durations
+            if(num_lines_marked == 0):
+                marked_ts_array[num_lines_marked] = [curr_time, curr_time]
+            else:
+                marked_ts_array.append([curr_time, curr_time-marked_ts_array[num_lines_marked-1][0]]) 
+                format_floats_for_table()
             last_marked_ts = time.time()
+            num_lines_marked += 1
             window["-NUM_LINES_MARKED-"].update(num_lines_marked)
         else:
             print("Dont click ""Mark"" too quickly! The line cannot be < 1 second long")
     time.sleep(0.01)    
     
-print(list_ts)
 window.close()
